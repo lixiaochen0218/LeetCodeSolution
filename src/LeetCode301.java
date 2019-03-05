76ca951c9a6185ab979d81c88c039ce0fc4fa63f
Xiaochen Li <lixiaochen0218@163.com>
2019-03-05 17:34:19 +1300

    fix solution

--- src/LeetCode301.java
+++ src/LeetCode301.java
@@ -2,11 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LeetCode301 {
+    class Solution {
         public List<String> removeInvalidParentheses(String s) {
             char[] chars = s.toCharArray();
             int left = 0;
             int right = 0;
-            for (int i=0;i<chars.length;i++) {
+            for (int i = 0; i < chars.length; i++) {
                 if (chars[i] == '(') {
                     left++;
                 } else if (chars[i] == ')' && left > 0) {
@@ -17,51 +18,63 @@ public class LeetCode301 {
 
                 }
             }
-            System.out.println("Remove  left:" + left + "  right:" + right);
 
-            int removeLeftIndex = 0;
+            int searchIndex = 0;
             List<String> answer = new ArrayList();
+            int count = s.length() - s.replace(")", "").length();
+            if (count == 0) {
+                s = s.replaceAll("\\(", "");
+                answer.add(s);
+                return answer;
+            }
+            System.out.println("Remove  left:" + left + "  right:" + right + " Count): " + count);
             loop1:
-            for (int j=0;j<chars.length;j++) {
+            while (searchIndex < s.length()) {
                 int leftToRemove = left;
                 int rightToRemove = right;
                 char[] removedChars = new char[chars.length];
-                System.arraycopy( chars, 0, removedChars, 0, chars.length );
+                System.arraycopy(chars, 0, removedChars, 0, chars.length);
                 boolean first = true;
-                System.out.println("start searching index: " + removeLeftIndex + String.valueOf(chars));
-                for (int i=removeLeftIndex;i<chars.length;i++) {
-                    if (i != 0 && removedChars[i] == removedChars[i-1]) {
+                System.out.println("start searching index: " + searchIndex + String.valueOf(chars));
+                for (int i = searchIndex; i < chars.length; i++) {
+                    if (i != 0 && removedChars[i] == removedChars[i - 1]) {
                         continue;
                     }
                     if (rightToRemove > 0 && removedChars[i] == ')') {
-                        removedChars[i] = ' ';
+                        removedChars[i] = '_';
                         rightToRemove--;
                         if (first) {
-                            removeLeftIndex = i+1;
+                            searchIndex = i + 1 - 1;
                             first = false;
                         }
 
-                    } else if (leftToRemove > 0 && removedChars[i] == '('){
-                        removedChars[i] = ' ';
+                    } else if (rightToRemove == 0 && leftToRemove > 0 && removedChars[i] == '(') {
+                        removedChars[i] = '_';
                         leftToRemove--;
                     } else {
-                        // ==0
-                        if (leftToRemove == 0 && rightToRemove ==0 ) {
-                            if (this.isValid(removedChars)) {
-                                answer.add(String.valueOf(removedChars).replaceAll(" ", ""));
-                                System.out.println("valid:" + String.valueOf(removedChars));
-                            } else {
-                                System.out.println("Invalid:" + String.valueOf(removedChars));
-                            }
-                            System.out.println("finish");
-                            break;
+
+                    }
+                    // ==0
+                    if (leftToRemove == 0 && rightToRemove == 0) {
+                        if (this.isValid(removedChars)) {
+                            answer.add(String.valueOf(removedChars).replaceAll("_", ""));
+                            System.out.println("valid:" + String.valueOf(removedChars));
+                        } else {
+                            System.out.println("Invalid:" + String.valueOf(removedChars));
                         }
+                        System.out.println("finish");
+                        break;
                     }
 
                 }
+                searchIndex++;
             }
-
-            return answer;
+            if (answer.size() == 0) {
+                answer.add(s);
+                return answer;
+            } else {
+                return answer;
+            }
 
 
         }
@@ -69,7 +82,7 @@ public class LeetCode301 {
         private boolean isValid(char[] chars) {
             int left = 0;
             int right = 0;
-            for (int i=0;i<chars.length;i++) {
+            for (int i = 0; i < chars.length; i++) {
                 if (chars[i] == '(') {
                     left++;
                 } else if (chars[i] == ')') {
@@ -88,4 +101,5 @@ public class LeetCode301 {
                 return false;
             }
         }
+    }
 }
