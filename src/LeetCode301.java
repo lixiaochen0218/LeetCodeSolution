bdba79ebd944a58ec7e6178bae10296cd0727929
Xiaochen Li <lixiaochen0218@163.com>
2019-03-05 23:07:27 +1300

    301 solution with recursion

--- src/LeetCode301.java
+++ src/LeetCode301.java
@@ -7,7 +7,7 @@ public class LeetCode301 {
             char[] chars = s.toCharArray();
             int left = 0;
             int right = 0;
-            for (int i = 0; i < chars.length; i++) {
+            for (int i=0;i<chars.length;i++) {
                 if (chars[i] == '(') {
                     left++;
                 } else if (chars[i] == ')' && left > 0) {
@@ -19,70 +19,45 @@ public class LeetCode301 {
                 }
             }
 
-            int searchIndex = 0;
+
             List<String> answer = new ArrayList();
-            int count = s.length() - s.replace(")", "").length();
-            if (count == 0) {
-                s = s.replaceAll("\\(", "");
-                answer.add(s);
-                return answer;
-            }
-            System.out.println("Remove  left:" + left + "  right:" + right + " Count): " + count);
-            loop1:
-            while (searchIndex < s.length()) {
-                int leftToRemove = left;
-                int rightToRemove = right;
-                char[] removedChars = new char[chars.length];
-                System.arraycopy(chars, 0, removedChars, 0, chars.length);
-                boolean first = true;
-                System.out.println("start searching index: " + searchIndex + String.valueOf(chars));
-                for (int i = searchIndex; i < chars.length; i++) {
-                    if (i != 0 && removedChars[i] == removedChars[i - 1]) {
-                        continue;
-                    }
-                    if (rightToRemove > 0 && removedChars[i] == ')') {
-                        removedChars[i] = '_';
-                        rightToRemove--;
-                        if (first) {
-                            searchIndex = i + 1 - 1;
-                            first = false;
-                        }
 
-                    } else if (rightToRemove == 0 && leftToRemove > 0 && removedChars[i] == '(') {
-                        removedChars[i] = '_';
-                        leftToRemove--;
-                    } else {
-
-                    }
-                    // ==0
-                    if (leftToRemove == 0 && rightToRemove == 0) {
-                        if (this.isValid(removedChars)) {
-                            answer.add(String.valueOf(removedChars).replaceAll("_", ""));
-                            System.out.println("valid:" + String.valueOf(removedChars));
-                        } else {
-                            System.out.println("Invalid:" + String.valueOf(removedChars));
-                        }
-                        System.out.println("finish");
-                        break;
-                    }
-
-                }
-                searchIndex++;
-            }
-            if (answer.size() == 0) {
-                answer.add(s);
-                return answer;
-            } else {
-                return answer;
-            }
-
+            System.out.println("Remove  left:" + left + "  right:" + right );
+            helper(s, answer, left , right, 0);
+            return answer;
 
         }
 
-        private boolean isValid(char[] chars) {
+        private void helper(String s, List<String> answer,int left,int right, int searchIndex) {
+            if (left == 0 && right == 0 && isValid(s)) {
+                answer.add(s);
+                return ;
+            }
+            for (int i = searchIndex; i< s.length(); i++) {
+                char ch = s.charAt(i);
+                if (i>0 && ch == s.charAt(i-1)){
+                    continue;
+                }
+                if (left > 0 && ch =='(')
+                {
+                    String new_str = s.substring(0,i) + s.substring(i+1);
+                    helper(new_str, answer, left -1, right, i);
+                    System.out.println("Remove  left:"+i + new_str );
+                }
+                if (right > 0 && ch ==')')
+                {
+                    String new_str = s.substring(0,i) + s.substring(i+1);
+                    helper(new_str, answer, left , right-1, i);
+                    System.out.println("Remove  right:" +i + new_str );
+                }
+            }
+        }
+
+        private boolean isValid(String s) {
+            char[] chars = s.toCharArray();
             int left = 0;
             int right = 0;
-            for (int i = 0; i < chars.length; i++) {
+            for (int i=0;i<chars.length;i++) {
                 if (chars[i] == '(') {
                     left++;
                 } else if (chars[i] == ')') {
