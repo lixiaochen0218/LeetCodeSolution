e4e31936ba909b7d341b168bc9bc979b53d3dcc7
Xiaochen Li <lixiaochen0218@163.com>
2019-04-01 15:37:31 +1300

    validation of BST

---
+++ src/LeetCode98.java
@@ -0,0 +1,27 @@
+public class LeetCode98 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return helper(root,null,null);
+    }
+
+    private boolean helper(TreeNode root, Integer max, Integer min){
+        if(root == null)
+            return true;
+        if(min != null && root.val <= min){
+            return false;
+        }
+        if(max != null && root.val >= max){
+            return false;
+        }
+
+        return helper(root.left, root.val, min) && helper(root.right, max, root.val);
+    }
+}
