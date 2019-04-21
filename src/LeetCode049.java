1baa9cb937e4a90436ebcf87d8c3ce05658a92ae
Xiaochen Li <lixiaochen0218@163.com>
2019-04-22 00:27:20 +1200

    HashMap2

--- src/LeetCode049.java
+++ src/LeetCode049.java
@@ -15,4 +15,29 @@ public class LeetCode049 {
         }
         return new ArrayList(map.values());
     }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> map = new HashMap<String, List>();
+        int[] counts = new int[26];
+        for(String s:strs){
+            Arrays.fill(counts, 0);
+            char[] chars = s.toCharArray();
+
+            for(char c:chars) counts[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+
+            for(int i=0;i<26;i++){
+                sb.append('#');
+                sb.append(counts[i]);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+    }
 }
