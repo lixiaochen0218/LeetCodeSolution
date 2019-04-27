388ac7f99b496d29fdb8d0620b5c2334c062fca8
Xiaochen Li <lixiaochen0218@163.com>
2019-04-27 15:04:54 +1200

    revere linkedlist

---
+++ src/LeetCode024.java
@@ -0,0 +1,19 @@
+public class LeetCode024 {
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null) || (head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/LeetCode206.java
@@ -0,0 +1,32 @@
+public class LeetCode206 {
+    //recursive
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+    //iterative
+    public ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
