bb2ce437edee57fc4c70733941fbaa460017f8b7
Xiaochen Li <lixiaochen0218@163.com>
2019-11-09 15:34:10 +1300

    inorder

---
+++ src/LeetCode099.java
@@ -0,0 +1,39 @@
+public class LeetCode099 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode pre;
+
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+        first = null;
+        second = null;
+        pre = null;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
