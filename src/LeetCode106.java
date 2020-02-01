b1d422879d49259872bd18f7d56117b3855cbcfc
Xiaochen Li <lixiaochen0218@163.com>
2020-02-01 18:46:07 +0800

    add TreeNode Class

--- src/LeetCode106.java
+++ src/LeetCode106.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 public class LeetCode106 {
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) {
-            val = x;
-        }
-    }
-
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         return buildTree(inorder, postorder, 0, inorder.length - 1, postorder.length - 1);
     }
